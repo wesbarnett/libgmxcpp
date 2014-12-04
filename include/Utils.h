dc89249db1ff62852fd62c48acb7240b93cace13
Wes Barnett <jbarnet4@tulane.edu>
2014-12-04 09:33:42 -0600

    update Utils to include coordinates and triclinicbox

--- include/Utils.h
+++ include/Utils.h
@@ -28,15 +28,73 @@ typedef vector < vector <double> > triclinicbox;
 
 /** Prints out coordinates cleanly with << */
 ostream& operator<<(ostream &os, coordinates xyz);
+
 /** Prints out box coordinates cleanly with << */
 ostream& operator<<(ostream &os, triclinicbox box);
 
-/** Prints out rvec coordinates cleanly with << */
-ostream& operator<<(ostream &os, rvec xyz);
-/** Prints out matrix (box) coordinates cleanly with << */
-ostream& operator<<(ostream &os, matrix box);
-
 /**
+ * @brief Adjusts for periodic boundary condition
+ * @details User passes a vector, most likely a vector pointing from one atom
+ * to another in the simulation. This function adjusts the vector such that if
+ * it is longer than 1/2 the box size it accounts for the periodic boundary.
+ * @param a Vector to be passed.
+ * @param box The box dimensions.
+ * @return Vector after pbc accounted for.
+ */
+coordinates pbc(coordinates a, triclinicbox box);
+
+/** @brief Calculates the cross product.
+ * @details Gets the cross product between vectors a and b and returns it.
+ * @param a First vector to be crossed.
+ * @param b Second vector to be crossed.
+ * @return The resultant vector of the cross of a and b.
+ */
+coordinates cross(coordinates a, coordinates b);
+
+/** @brief Calculates the distance between atom a and atom b.
+ * @return Distance
+ * @param a First atom in distance calculation
+ * @param b Second atom in distance calculation
+ * @param box Box dimensions
+ */
+double distance(coordinates a, coordinates b, triclinicbox box);
+
+/** @brief Calculates the distance squared between atom a and atom b.
+ * @return Distance
+ * @param a First atom in distance squared calculation
+ * @param b Second atom in distance squared calculation
+ * @param box Box dimensions
+ */
+double distance2(coordinates a, coordinates b, triclinicbox box);
+
+/** @brief Calculates the dot product between two vectors
+ * @param a First vector in dot product
+ * @param b Second vector in dot product
+ * @return Dot product of a and b
+ */
+double dot(coordinates a, coordinates b);
+
+/** @brief Calculates the magnitude of a vector
+ * @return Magnitude
+ * @param x Vector for which magnitude is desired
+ */
+double magnitude(coordinates x);
+
+/** @brief Checks if a file exists
+ * @param filename Name of the file to check
+ * @return Whether or not the file exists
+ */
+bool fileExists(string filename);
+
+/* @brief Calculates the volume of simulation box
+ * @param box Box dimensions
+ * @return Volume of box
+ */
+double volume(triclinicbox box);
+
+
+/* OLD FUNCTIONS - kept for backwards compatilibity */
+/*
  * @brief Adjusts for periodic boundary condition
  * @details User passes a vector, most likely a vector pointing from one atom
  * to another in the simulation. This function adjusts the vector such that if
@@ -45,69 +103,70 @@ ostream& operator<<(ostream &os, matrix box);
  * @param box The box dimensions.
  */
 void pbc(rvec a, matrix box);
-/** @brief Calculates the cross product.
+/* @brief Calculates the cross product. OLD
  * @details Gets the cross product between vectors a and b and returns r.
  * @param a First vector to be crossed.
  * @param b Second vector to be crossed.
  * @param r The resultant vector of the cross of a and b.
  */
 void cross(rvec a, rvec b, rvec r);
-/** @brief Calculates the distance between atom a and atom b.
+/* @brief Calculates the distance between atom a and atom b.
  * @return Distance
  * @param a First atom in distance calculation
  * @param b Second atom in distance calculation
  * @param box Box dimensions
  */
 double distance(rvec a, rvec b, matrix box);
-/** @brief Calculates the distance squared between atom a and atom b.
+/* @brief Calculates the distance squared between atom a and atom b.
  * @return Distance
  * @param a First atom in distance squared calculation
  * @param b Second atom in distance squared calculation
  * @param box Box dimensions
  */
 double distance2(rvec a, rvec b, matrix box);
-/** @brief Calculates the magnitude of a vector
+/* @brief Calculates the magnitude of a vector
  * @return Magnitude
  * @param x Vector for which magnitude is desired
  */
 double magnitude(rvec x);
-/** @brief Calculates the volume of simulation box
+/* @brief Calculates the volume of simulation box
  * @param box Box dimensions
  * @return Volume of box
  */
 double volume(matrix box);
-/** @brief Calculates the dot product between two vectors
+/* @brief Calculates the dot product between two vectors
  * @param a First vector in dot product
  * @param b Second vector in dot product
  * @return Dot product of a and b
  */
 double dot(rvec a, rvec b);
-/** @brief Checks if a file exists
- * @param filename Name of the file to check
- * @return Whether or not the file exists
- */
-bool fileExists(string filename);
-/** @brief Adds two vectors together
+/* @brief Adds two vectors together
  * @param a First vector to be added
  * @param b Second vector to be added
  * @param c Result of a + b
  */
 void add(rvec a, rvec b, rvec c);
-/** @brief Subtracts two vectors
+/* @brief Subtracts two vectors
  * @param a First vector to be subtracted
  * @param b Second vector to be subtracted
  * @param c Result of a - b
  */
 void subtract(rvec a, rvec b, rvec c);
-/** @brief Multiplies a vector by a constant
+/* @brief Multiplies a vector by a constant
  * @param a Vector to be multiplied. Returned as a * x.
  * @param x Constant by which to multiply.
  */
 void multiply(rvec a, double x);
-/** @brief Divides a vector by a constant
+/* @brief Divides a vector by a constant
  * @param a Vector to be divided. Returned as a / x.
  * @param x Constant by which to divide
  */
 void divide(rvec a, double x);
+
+/* Prints out rvec coordinates cleanly with << */
+ostream& operator<<(ostream &os, rvec xyz);
+/* Prints out matrix (box) coordinates cleanly with << */
+ostream& operator<<(ostream &os, matrix box);
+
 
 #endif
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -5,6 +5,92 @@
 
 #include "Utils.h"
 
+coordinates pbc(coordinates a, triclinicbox box) {
+
+	coordinates b;
+    vector <double> box_inv(3);
+    double shift;
+
+	b = a;
+
+    box_inv.at(X) = ((float)1.0)/box.at(X).at(X);
+    box_inv.at(Y) = ((float)1.0)/box.at(Y).at(Y);
+    box_inv.at(Z) = ((float)1.0)/box.at(Z).at(Z);
+
+    shift = round(b.at(Z) * box_inv.at(Z));
+    b.at(Z) -= box.at(Z).at(Z) * shift;
+    b.at(Y) -= box.at(Z).at(Y) * shift;
+    b.at(X) -= box.at(Z).at(X) * shift;
+
+    shift = round(b.at(Y) * box_inv.at(Y));
+    b.at(Y) -= box.at(Y).at(Y) * shift;
+    b.at(X) -= box.at(Y).at(X) * shift;
+
+    shift = round(b.at(X) * box_inv.at(X));
+    b.at(X) -= box.at(X).at(X) * shift;
+
+    return b;
+
+}
+
+coordinates cross(coordinates a, coordinates b) {
+	coordinates r;
+	r.at(X) = a.at(Y)*b.at(Z)-a.at(Z)*b.at(Y);
+	r.at(Y) = a.at(Z)*b.at(X)-a.at(X)*b.at(Z);
+	r.at(Z) = a.at(X)*b.at(Y)-a.at(Y)*b.at(X);
+}
+
+double distance2(coordinates a, coordinates b, triclinicbox box) {
+
+    coordinates c;
+    c.at(X) = a.at(X) - b.at(X);
+    c.at(Y) = a.at(Y) - b.at(Y);
+    c.at(Z) = a.at(Z) - b.at(Z);
+    pbc(c,box);
+
+    return dot(c,c);
+
+}
+
+double distance(coordinates a, coordinates b, triclinicbox box) {
+    return sqrt(distance2(a,b,box));
+}
+
+double dot(coordinates a, coordinates b) {
+    return a.at(X)*b.at(X)+a.at(Y)*b.at(Y)+a.at(Z)*b.at(Z);
+}
+
+double magnitude(coordinates x) {
+    return sqrt(dot(x,x));
+}
+
+double volume(triclinicbox box) {
+    return (box.at(X).at(X)*box.at(Y).at(Y)*box.at(Z).at(Z) + \
+            box.at(X).at(Y)*box.at(Y).at(Z)*box.at(Z).at(X) + \
+            box.at(X).at(Z)*box.at(Y).at(X)*box.at(Z).at(Y) - \
+            box.at(X).at(Z)*box.at(Y).at(Y)*box.at(Z).at(X) + \
+            box.at(X).at(Y)*box.at(Y).at(X)*box.at(Z).at(Z) + \
+            box.at(X).at(X)*box.at(Y).at(Z)*box.at(Z).at(Y));
+}
+
+ostream& operator<<(ostream &os, coordinates xyz) {
+    os << xyz.at(X) << " " << xyz.at(Y) << " " << xyz.at(Z) << endl;
+    return os;
+}
+
+ostream& operator<<(ostream &os, triclinicbox box) {
+	for (int j=0; j<DIM; j++) {
+		for (int k=0; k<DIM; k++) {
+			os << box.at(j).at(k) << " ";
+        }
+        os << endl;
+	}
+	os << endl;
+    return os;
+}
+
+
+/* Old functions below */
 void pbc(rvec a, matrix box) {
 
     rvec box_inv;
@@ -104,22 +190,6 @@ void multiply(rvec a, double x) {
 
 ostream& operator<<(ostream &os, rvec xyz) {
     os << xyz[X] << " " << xyz[Y] << " " << xyz[Z] << endl;
-    return os;
-}
-
-ostream& operator<<(ostream &os, coordinates xyz) {
-    os << xyz.at(X) << " " << xyz.at(Y) << " " << xyz.at(Z) << endl;
-    return os;
-}
-
-ostream& operator<<(ostream &os, triclinicbox box) {
-	for (int j=0; j<DIM; j++) {
-		for (int k=0; k<DIM; k++) {
-			os << box.at(j).at(k) << " ";
-        }
-        os << endl;
-	}
-	os << endl;
     return os;
 }
 
