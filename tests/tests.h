0dcc7b9c441b10f8de577b0d83e48429c2e31add
James Barnett <jbarnet4@tulane.edu>
2015-04-06 12:55:01 -0500

    update comparison test

--- tests/tests.h
+++ tests/tests.h
@@ -1,10 +1,14 @@
 
+#include <cmath>
 #include <math.h>
+#include <iostream>
 
 double const TOL = pow(10,-5);
 
 bool test_equal(double a, double b)
 {
-    bool test = (a >= b-TOL && a <= b+TOL);
+    std::cout << abs(a-b) << std::endl;
+    bool test = (std::abs(a-b) <= TOL);
+    std::cout << test << std::endl;
     return test;
 }
