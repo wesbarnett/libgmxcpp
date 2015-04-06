611e90caedff6fe5b7bb434c93a1573b3906b363
James Barnett <jbarnet4@tulane.edu>
2015-04-06 15:38:19 -0500

    add surface area calc tests

--- tests/Utils.cpp
+++ tests/Utils.cpp
@@ -4,11 +4,13 @@
 #include "gmxcpp/coordinates.h"
 #include "gmxcpp/triclinicbox.h"
 #include "gmxcpp/Utils.h"
+#include <iomanip>
 
 int main() 
 {
 
     /* Test some utils*/
+    srand(0);
     triclinicbox b1(3.5,4.5,4.0);
 
     coordinates c6(3.6,4.7,5.0);
@@ -30,6 +32,30 @@ int main()
 
     assert(test_equal(distance(c8,c9,b2),0.0));
     assert(test_equal(distance(c6,c8,b1),2.33452));
+
+    vector <coordinates> sites;
+    triclinicbox b3(10.0,10.0,10.0);
+    coordinates site1(1.0,1.0,1.0);
+    sites.push_back(site1);
+    assert(test_equal(get_surf_area(sites, 1.0, 10000, b3),12.56637061));
+
+    coordinates site2(1.0,1.0,4.0);
+    sites.push_back(site2);
+    assert(test_equal(get_surf_area(sites, 1.0, 10000, b3),25.13274123));
+
+    /*
+     * TODO: fix these
+    sites.at(1).at(X) = 1.0;
+    sites.at(1).at(Y) = 1.0;
+    sites.at(1).at(Z) = 1.5;
+    cout << std::setprecision(12) << get_surf_area(sites, 1.0, 10000, b3);
+    assert(test_equal(get_surf_area(sites, 1.0, 10000, b3)-15.77456));
+    
+    coordinates site3(1.0,1.0,2.0);
+    sites.push_back(site3);
+    cout << std::setprecision(12) << get_surf_area(sites, 1.0, 10000, b3);
+    assert(test_equal(get_surf_area(sites, 1.0, 10000, b3),18.7641));
+    */
 
 	return 0;
 }
--- tests/tests.h
+++ tests/tests.h
@@ -7,8 +7,6 @@ double const TOL = pow(10,-5);
 
 bool test_equal(double a, double b)
 {
-    std::cout << abs(a-b) << std::endl;
     bool test = (std::abs(a-b) <= TOL);
-    std::cout << test << std::endl;
     return test;
 }
