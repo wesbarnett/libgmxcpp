36e0d138586cc2b095c3d048bd679138aff589ac
James Barnett <jbarnet4@tulane.edu>
2015-04-08 11:10:48 -0500

    remove unnecessary files

--- cmake/docs/CMakeFiles/my_project_docs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/my_project_docs"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/my_project_docs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
