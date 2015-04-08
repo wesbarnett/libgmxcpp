df03c4e0bb1c549887e4a0cef7b14a7865ede166
James Barnett <jbarnet4@tulane.edu>
2015-04-08 12:53:19 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@
 import sys
 import os
 import shlex
+import subprocess
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -138,7 +139,7 @@ todo_include_todos = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -284,15 +285,15 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-#sys.path.append( "/var/build/user_builds/libgmxcpp/checkouts/latest/ext/breathe" )
+# Needed for breathe, which links doxygen xml file to sphinx
 extensions = ['breathe' ]
 breathe_projects = { "libgmxcpp":"./doxyxml" }
 breathe_default_project = "libgmxcpp"
 
-import subprocess, os
-
+# Runs doxygen on code
 subprocess.call('cd ..; doxygen; cd docs', shell=True)
 
+# Are we on readthedocs?
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
