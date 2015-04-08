8a0afbd1a574d03502c1390fb798aa8c639f389e
James Barnett <jbarnet4@tulane.edu>
2015-04-08 11:12:00 -0500

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -293,9 +293,10 @@ import subprocess, os
 
 subprocess.call('cd ..; doxygen; cd docs', shell=True)
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if read_the_docs_build:
-
-        subprocess.call('cd ..; doxygen; cd docs', shell=True)
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
