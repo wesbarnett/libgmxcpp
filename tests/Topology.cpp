2d7a1c2c85389da8e9ebee5f5ad1b54580bcfcaa
James Barnett <jbarnet4@tulane.edu>
2015-04-11 08:17:23 -0500

    add more Topology tests

--- tests/Topology.cpp
+++ tests/Topology.cpp
@@ -2,19 +2,18 @@
 #include "tests.h"
 #include "gmxcpp/Topology.h"
 
-#include <iostream>
-using namespace std;
-
-
 int main()
 {
-    //TODO: Add more tests for charges with index groups.
-    //TODO: Add tests for masses
     Index ndx("tests/test.ndx");
     Topology t1("tests/test.tpr",ndx);
 
     assert(test_equal(t1.GetCharge(0),-2.40000e-01));
     assert(test_equal(t1.GetCharge(99),0.52422));
+    assert(test_equal(t1.GetCharge(3,"SOL"),-1.04844));
+
+    assert(test_equal(t1.GetMass(0),1.20110e+01));
+    assert(test_equal(t1.GetMass(99),1.008));
+    assert(test_equal(t1.GetMass(3,"SOL"),0.00000));
 
     return 0;
 }
