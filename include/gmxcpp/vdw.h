e11c70a0ec90ab14fb92c779e19dccfcbd931e16
James W. Barnett <jbarnet4@tulane.edu>
2015-09-04 21:36:37 -0500

    fix vdw radii map

--- include/gmxcpp/vdw.h
+++ include/gmxcpp/vdw.h
@@ -5,10 +5,11 @@
 #include <map>
 #include <string>
 
-std::map<std::string,double> VDW_RADII;
-VDW_RADII["H"] = 0.12;
-VDW_RADII["C"] = 0.17;
-VDW_RADII["N"] = 0.155;
-VDW_RADII["O"] = 0.152;
+std::map<std::string,double> VDW_RADII = {
+    {"H", 0.12},
+    {"C", 0.17},
+    {"N", 0.155},
+    {"O", 0.152}
+};
 
 #endif
