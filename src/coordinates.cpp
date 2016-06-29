3fcc75b9a3ab4bc52048fe42d2c4e4e7d7321ed1
James W. Barnett <jbarnet4@tulane.edu>
2016-06-29 10:46:30 -0500

    Revert "simpler = for coordinates"
    This reverts commit f121bf728f25f17f38301bafccd621c7e121bd7e.

--- src/coordinates.cpp
+++ src/coordinates.cpp
@@ -34,7 +34,9 @@ coordinates::coordinates(){ }
 
 coordinates::coordinates(double x, double y, double z)
 {
-    this->r = {x, y, z};
+    this->r[X] = x;
+    this->r[Y] = y;
+    this->r[Z] = z;
 }
 
 double& coordinates::operator[](int i)
@@ -49,7 +51,9 @@ const double& coordinates::operator[](int i) const
 
 void coordinates::set(double x, double y, double z)
 {
-    this->r = {x, y, z};
+    this->r[X] = x;
+    this->r[Y] = y;
+    this->r[Z] = z;
     return;
 }
 
